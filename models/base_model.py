ff6f2030df635c26d1cdc086ac1a7f59605d82f4
maba-dev <cherif.ba@hotmail.fr>
2022-03-01 11:14:38 +0100

    task 4 we created a method to generate a dictionary representation of an instance (method to_dict()).

--- models/base_model.py
+++ models/base_model.py
@@ -5,14 +5,22 @@
 
 import uuid
 from datetime import datetime
-import models
 class BaseModel:
     """ define the class"""
-    def __init__(self):
-    
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        val = datetime.strptime(value,'%Y-%m-%dT%H:%M:%S.%f')
+                        setattr(self, key, val)
+                    else:
+                        setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
 
     def __str__(self):
