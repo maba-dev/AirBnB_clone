96e996e99eab76ecf86c11427a24af95134fee61
Astambar <leuchartjason@gmail.com>
2022-03-01 15:27:46 +0000

    correctif file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,6 @@ class FileStorage:
     """Class for converting JSON to base class and vice versa."""
     __file_path = "file.json"
     __objects = {}
-    id=12121212
 
     def all(self):
         """Returns __objects dictionary."""
@@ -23,7 +22,7 @@ class FileStorage:
 
     def save(self):
         """Convert objects to JSON file."""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as filest:
+        with open(FileStorage.__file_path, "w+", encoding="utf-8") as filest:
             dictio = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(dictio, filest)
 
@@ -32,8 +31,13 @@ class FileStorage:
         """Transform the JSON file into objects."""
         if not os.path.isfile(FileStorage.__file_path):
             return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as filest:
+        with open(FileStorage.__file_path, "r+", encoding="utf-8") as filest:
             obj_dict = json.load(filest)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
             FileStorage.__objects = obj_dict
+    def classes(self):
+        """Retourn le  dictionaires des classes de référence"""
+        from models.base_model import BaseModel
+
+        return {"BaseModel": BaseModel}
