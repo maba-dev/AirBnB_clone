ea2d9210f3e37a381a4b2293e3770576ddc12baa
maba-dev <cherif.ba@hotmail.fr>
2022-03-01 10:10:03 +0100

    a class BaseModel that defines all common attributes/methods for other classes:

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    a class BaseModel that defines all common attributes/methods for other classes
+ """
+
+import uuid
+from datetime import datetime
+import models
+class BaseModel:
+    """ define the class"""
+    def __init__(self):
+    
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+
+    def __str__(self):
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    
+
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary.update({'__class__': type(self).__name__})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
