d262895f015bd12d06303ce6f21f76ab06340f75
maba-dev <cherif.ba@hotmail.fr>
2022-03-04 14:34:27 +0100

    add the global_usage

---
+++ global_usage.py
@@ -0,0 +1,17 @@
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
+classe = {
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User" : User,
+    "Amenity": Amenity
+        }
