a6e93b19ad6b0db80240a5498e16e680ae0e68ac
maba-dev <cherif.ba@hotmail.fr>
2022-03-06 11:44:06 +0100

    correct the pep8 of the class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-    a class BaseModel that defines all common attributes/methods for other classes
+    a class BaseModel that defines all common
+    attributes/methods for other classes
  """
-
 import uuid
 from datetime import datetime
 import models
+
+
 class BaseModel:
     """ define the class"""
     def __init__(self, *args, **kwargs):
+        """ initialize the  constructor"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
-                        setattr(self, key,
-                                datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                                )
+                        setattr(self, key, datetime.strptime(value,
+                        '%Y-%m-%ddT%H:%M:%S.%f'))
                     else:
                         setattr(self, key, value)
         else:
@@ -25,19 +27,21 @@ class BaseModel:
             models.storage.new(self)
             models.storage.save()
 
-
     def __str__(self):
-        
+        """
+            The special __str__ method is used to indicate
+            the string representation of an object.
+        """
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__,
                                          self.id, self.__dict__)
 
-    
-
     def save(self):
+        """call save(self) method of storage """
         self.updated_at = datetime.now()
         models.storage.save()
-    
+
     def to_dict(self):
+        """ a method to generate a dictionary representation of an instance"""
         dictionary = self.__dict__.copy()
         dictionary.update({'__class__': type(self).__name__})
         dictionary['created_at'] = self.created_at.isoformat()
