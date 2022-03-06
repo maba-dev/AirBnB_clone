337664e530b78493adb80979a56564169bf8d755
maba-dev <cherif.ba@hotmail.fr>
2022-03-06 12:42:15 +0100

    correct the pep8 of the console

--- console.py
+++ console.py
@@ -115,8 +115,9 @@ class HBNBCommand(cmd.Cmd):
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                list_item = [str(obj) for key, obj in storage.all().items()
-                    if type(obj).__name__ == words[0]]
+                list_item = [str(obj) for key,
+                             obj in storage.all().items()
+                             if type(obj).__name__ == words[0]]
                 print(list_item)
         else:
             list_item = [str(obj) for key, obj in storage.all().items()]
